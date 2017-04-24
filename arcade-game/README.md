a584cf2b60ab785785baa9631aa82455da658c46
id7oo <ded7477@gmail.com>
2017-04-24 10:06:28 +0300

    arcade game

--- arcade-game/README.md
+++ arcade-game/README.md
@@ -1,10 +1,12 @@
+# Arcade Game
+> Submitted to Udacity
+[![N|Solid](https://d125fmws0bore1.cloudfront.net/assets/favicon-3867f43151ecbff486335ae6a96d17d91d6169d20888131b4681d15b65e1425b.ico)](https://www.udacity.com/) 
 
-How do you run this game?
+# How do you run this game?
 
-Download the project folders.
-Open index.html in a any browser.
+- Download the project folders.
+- Open index.html in a any browser.
 
-How do you play?
+# How do you play?
 
-Use the arrow keys to move around. Try not to hit the bugs, otherwise you respawn at the starting point. If you are able to get to the blue area, you are winner!
-
+> Use the arrow keys to move around. Try not to hit the bugs, otherwise you respawn at the starting point. If you are able to get to the blue area, you are winner!
--- arcade-game/js/app.js
+++ arcade-game/js/app.js
@@ -1,3 +1,4 @@
+
 // Enemies our player must avoid
 var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
@@ -8,25 +9,30 @@ var Enemy = function(x, y) {
     this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
-    this.speed = Math.floor((Math.random() * 200) + 200);;
-    this.height = 80;
-    this.width = 30;
+    this.speed = Math.floor((Math.random() * 200) + 100);
+};
 
-
+Object.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-//updating the enemy's speed
-    this.x += this.speed * dt;
-
-    if (this.x > 600) {
-        this.x = -100;
+    if (this.x <= 550) {
+        this.x += this.speed * dt;
+    } else {
+        this.x = -2;
     }
+    if (player.x >= this.x - 30 && player.x <= this.x + 30) {
+        if (player.y >= this.y - 30 && player.y <= this.y + 30) {
+            player.reset();
+        }
+    }
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
 };
-
-
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
@@ -34,102 +40,58 @@ Enemy.prototype.render = function() {
 };
 
 // Now write your own player class
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.x = 200;
+    this.y = 400;
+};
+Player.prototype.reset = function() {
+    this.x = 200;
+    this.y = 400;
+};
 // This class requires an update(), render() and
 // a handleInput() method.
-//Player's character image
-var Player = function(x, y) {
-    this.sprite = 'images/char-boy.png';
-    this.x = x;
-    this.y = y;
-    this.height = 30;
-    this.width = 80;
+Player.prototype.update = function() {
+    //if left key is pressed and player is not on edge of map, pressed decrement x
+    if (this.ctlKey === 'left' && this.x > 0) {
+        this.x = this.x - 50;
+        //if right key is pressed and player is not on edge of map increment x 
+    } else if (this.ctlKey === 'right' && this.x != 400) {
+        this.x = this.x + 50;
+        //if up key is pressed increment y 
+    } else if (this.ctlKey === 'up') {
+        this.y = this.y - 50;
+        //if down key is pressed and player is not on edge of map decrement y 
+    } else if (this.ctlKey === 'down' && this.y != 400) {
+        this.y = this.y + 50;
+    }
+    this.ctlKey = null;
 
+    //If on water, reset
+    if (this.y < 25) {
+        alert("Gongrats, You win!!");
+        this.reset();
+    }
 };
 
-//Player should not go beyond boundaries
-//If player win it will show in pop-up window.
-Player.prototype.update = function(dt) {
-    this.x * (dt);
-    this.y * (dt);
-
-    if (this.x > 400) {
-        this.x = 0;
-    }
-
-    if (this.x < -50) {
-        this.x = 0;
-    }
-
-    if (this.y < 0) {
-        alert("you win!!");
-        this.y = 400;
-    }
-
-    if (this.y > 400) {
-        this.y = 400;
-    }
-
-
+Player.prototype.handleInput = function(e) {
+    this.ctlKey = e;
 };
-
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-Player.prototype.handleInput = function(direction) {
-
-    if (direction === 'left') {
-        this.x = this.x - 100;
-    }
-    if (direction === 'up') {
-        this.y = this.y - 100;
-    }
-
-    if (direction === 'right') {
-        this.x = this.x + 100;
-    }
-
-    if (direction === 'down') {
-        this.y = this.y + 100;
-    }
-
-
-};
-
-
-
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+// All eneimeies will come out from -5.
 var allEnemies = [];
-allEnemies[0] = new Enemy(100, 200);
-allEnemies[1] = new Enemy(100, 100);
-allEnemies[2] = new Enemy(100, 60);
+(function setEnemies() {
+    // allEnemies.push(new Enemy(-5, 50)); more difficulty.
+    allEnemies.push(new Enemy(-5, 100));
+    allEnemies.push(new Enemy(-5, 150));
+    allEnemies.push(new Enemy(-5, 200));
+    // allEnemies.push(new Enemy(-5, 250)); more difficulty.
+}());
 
-
-var player = new Player(50, 400);
-
-
-
-Player.prototype.resetPlayer = function() {
-    this.x = 0;
-    this.y = 420;
-};
-
-Player.prototype.checkCollisions = function(allEnemies) {
-    var player = this;
-    console.log(this);
-    for (var i = 0; i < allEnemies.length; i++) {
-        if (allEnemies[i].x < this.width + this.x && allEnemies[i].width + allEnemies[i].x > this.x && allEnemies[i].y < this.y + this.height && allEnemies[i].height + allEnemies[i].y > this.y) {
-            player.resetPlayer();
-        }
-
-    }
-};
-
-
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
